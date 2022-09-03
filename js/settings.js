437e11c475ea3e26598a581e971876ef2488ca3d
IdaRandy <112627013+IdaRandy@users.noreply.github.com>
2022-09-03 13:19:25 +0800

    DukcapilLobar

---
+++ js/settings.js
@@ -0,0 +1,19 @@
+{
+    "workbench.colorTheme": "Default High Contrast",
+    "open-php-html-js-in-browser.selectedBrowser": "Chrome",
+    "php.debug.executablePath": "",
+    "security.workspace.trust.untrustedFiles": "newWindow",
+    "files.autoSave": "afterDelay",
+    "grunt.autoDetect": "on",
+    "launch": {
+        "configurations": [
+        {
+            "name": "Attach to Chrome",
+            "port": 9222,
+            "request": "attach",
+            "type": "chrome",
+            "webRoot": "${workspaceFolder}"
+        }
+        ]
+}
+}
